f6345cca6c2756fa75c6b8d6798a7f1925f07678
Alexey Bitkov <alxsmrn@yandex.ru>
2023-04-18 21:05:45 +0300

    delete redundant request check

--- internal/controller/orders_handler.go
+++ internal/controller/orders_handler.go
@@ -16,21 +16,10 @@ type ordersHandler struct {
 	processor ordersProcessor
 }
 
-func (o *ordersHandler) checkRequestFormat() error {
-	return nil
-}
 func (o *ordersHandler) CreateOrder(c *gin.Context) {
-	err := o.checkRequestFormat()
-	if err != nil {
-		return
-	}
 }
 
 func (o *ordersHandler) GetOrders(c *gin.Context) {
-	err := o.checkRequestFormat()
-	if err != nil {
-		return
-	}
 }
 
 func NewOrdersHandler(processor ordersProcessor) *ordersHandler {
