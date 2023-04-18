6c20a4d7e61a7416edb02c36ee4f329ffa0fe46f
Alexey Bitkov <alxsmrn@yandex.ru>
2023-04-18 21:03:41 +0300

    implement `login` use case

--- internal/usecase/user_authenticator.go
+++ internal/usecase/user_authenticator.go
@@ -2,41 +2,62 @@ package usecase
 
 import (
 	"context"
-	"crypto/sha256"
-	"encoding/hex"
+	"time"
 
-	"github.com/google/uuid"
+	"github.com/golang-jwt/jwt/v4"
+
+	"github.com/Albitko/loyalty-program/internal/entities"
+	"github.com/Albitko/loyalty-program/internal/utils"
 )
 
 type userRepository interface {
 	Register(ctx context.Context, id, login, hashedPassword string) error
-	GetCredentials(ctx context.Context, login string) (string, error)
+	GetCredentials(ctx context.Context, login string) (entities.User, error)
 }
 
 type authenticator struct {
 	repository userRepository
+	secret     string
 }
 
-func (a *authenticator) CheckIsLoginFree(ctx context.Context, login string) error {
-	_, err := a.repository.GetCredentials(ctx, login)
+func (a *authenticator) CreateAccessToken(user entities.User) (string, error) {
+	exp := time.Now().Add(time.Hour).Unix()
+	claims := &entities.JwtCustomClaims{
+		Name: user.Login,
+		ID:   user.ID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: exp,
+		},
+	}
+	unsignedToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := unsignedToken.SignedString([]byte(a.secret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+func (a *authenticator) Register(ctx context.Context, user entities.User) error {
+	err := a.repository.Register(ctx, user.ID, user.Login, utils.HexHash(user.Password))
 	return err
 }
 
-func (a *authenticator) Register(ctx context.Context, login, password string) error {
-	id := uuid.New().String()
-	hash := sha256.New()
-	hash.Write([]byte(password))
-	err := a.repository.Register(ctx, id, login, hex.EncodeToString(hash.Sum(nil)))
-	return err
+func (a *authenticator) Auth(ctx context.Context, login, password string) (entities.User, error) {
+
+	passwordHash := utils.HexHash(password)
+	user, err := a.repository.GetCredentials(ctx, login)
+	if err != nil {
+		return user, err
+	}
+	if passwordHash != user.Password {
+		return user, entities.ErrInvalidCredentials
+	}
+	return user, nil
 }
 
-func (a *authenticator) Auth(ctx context.Context) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func NewAuthenticator(repository userRepository) *authenticator {
+func NewAuthenticator(repository userRepository, secret string) *authenticator {
 	return &authenticator{
 		repository: repository,
+		secret:     secret,
 	}
 }
