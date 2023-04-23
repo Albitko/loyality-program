3ae1d0d97b47d8577e0e559ca10b65dd031e0639
Alexey Bitkov <alxsmrn@yandex.ru>
2023-04-23 20:48:30 +0300

    implement accrual service getter

--- internal/workers/accrual_getter.go
+++ internal/workers/accrual_getter.go
@@ -2,6 +2,7 @@ package workers
 
 import (
 	"github.com/Albitko/loyalty-program/internal/entities"
+	"github.com/Albitko/loyalty-program/internal/utils"
 )
 
 type accrualGetter struct {
@@ -10,6 +11,14 @@ type accrualGetter struct {
 
 func (s *accrualGetter) GetAccrual(orderID string) (entities.Order, error) {
 	var order entities.Order
+	_, err := utils.RestyClient.R().
+		EnableTrace().
+		SetResult(&order).
+		Get(s.accrualURL + "/api/orders/" + orderID)
+
+	if err != nil {
+		return order, err
+	}
 	return order, nil
 }
 
